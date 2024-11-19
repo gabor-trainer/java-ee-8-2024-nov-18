5c0d03cffd908ba0af8d781ff72fef6cfc96030f
github will ignore this <gabor.trainer@outlook.com>
2024-11-19 11:36:17 +0100

    feat[demo]: imploreved solution for the exercise: custommess

--- demos/jsfcustommess/src/main/java/net/ensode/javaee8book/jsfcustommess/AgeValidator.java
+++ demos/jsfcustommess/src/main/java/net/ensode/javaee8book/jsfcustommess/AgeValidator.java
@@ -20,6 +20,33 @@ public class AgeValidator implements Validator
         HtmlInputText htmlInputText = (HtmlInputText) uiComponent;
 
 
+        int age = (int) value;
+        if (age <= 18) {
+            FacesMessage facesMessage = new FacesMessage(htmlInputText.getLabel()
+                    + ": Age must be over 18");
+            throw new ValidatorException(facesMessage);
+        }
 
+
+//        String ageString = (String) value;
+//
+//        if (ageString == null || ageString.trim().isEmpty()) {
+//            FacesMessage facesMessage = new FacesMessage(htmlInputText.getLabel()
+//                    + ": Age field cannot be empty");
+//            throw new ValidatorException(facesMessage);
+//        }
+//
+//        try {
+//            int age = Integer.parseInt(ageString);
+//            if (age <= 18) {
+//                FacesMessage facesMessage = new FacesMessage(htmlInputText.getLabel()
+//                        + ": Age must be over 18");
+//                throw new ValidatorException(facesMessage);
+//            }
+//        } catch (NumberFormatException e) {
+//            FacesMessage facesMessage = new FacesMessage(htmlInputText.getLabel()
+//                    + ": Age must be a numeric value");
+//            throw new ValidatorException(facesMessage);
+//        }
     }
 }
